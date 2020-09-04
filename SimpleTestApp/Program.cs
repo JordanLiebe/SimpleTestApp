a509734c2611975733b8d2470a0bf347aa81ddec
Jordan M. Liebe <Jordan.Liebe@outlook.com>
2020-09-04 14:27:51 -0700

    Initial Commit

---
+++ SimpleTestApp/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace SimpleTestApp
+{
+    public static class Math
+    {
+        public static int Addition(int X, int Y)
+        {
+            return X + Y;
+        }
+
+        public static int Subtraction(int X, int Y)
+        {
+            return X - Y;
+        }
+
+        public static int Multiplication(int X, int Y)
+        {
+            return X * Y;
+        }
+
+        public static int Division(int X, int Y)
+        {
+            return X / Y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            string command;
+
+            Console.WriteLine("Enter a Command (Add, Sub, Div, Mult)");
+
+            while(run)
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+                switch(command)
+                {
+                    case "a":
+                    case "add":
+                    case "Add":
+                        try
+                        {
+                            int Input1, Input2 = -1;
+
+                            Console.Write("1st Input: ");
+                            Input1 = Int32.Parse(Console.ReadLine());
+
+                            Console.Write("2nd Input: ");
+                            Input2 = Int32.Parse(Console.ReadLine());
+
+                            int ReturnValue = Math.Addition(Input1, Input2);
+                            Console.WriteLine("Return: " + ReturnValue);
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "s":
+                    case "sub":
+                    case "Sub":
+                        try
+                        {
+                            int Input1, Input2 = -1;
+
+                            Console.Write("1st Input: ");
+                            Input1 = Int32.Parse(Console.ReadLine());
+
+                            Console.Write("2nd Input: ");
+                            Input2 = Int32.Parse(Console.ReadLine());
+
+                            int ReturnValue = Math.Subtraction(Input1, Input2);
+                            Console.WriteLine("Return: " + ReturnValue);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "m":
+                    case "mult":
+                    case "Mult":
+                        try
+                        {
+                            int Input1, Input2 = -1;
+
+                            Console.Write("1st Input: ");
+                            Input1 = Int32.Parse(Console.ReadLine());
+
+                            Console.Write("2nd Input: ");
+                            Input2 = Int32.Parse(Console.ReadLine());
+
+                            int ReturnValue = Math.Multiplication(Input1, Input2);
+                            Console.WriteLine("Return: " + ReturnValue);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "d":
+                    case "div":
+                    case "Div":
+                        try
+                        {
+                            int Input1, Input2 = -1;
+
+                            Console.Write("1st Input: ");
+                            Input1 = Int32.Parse(Console.ReadLine());
+
+                            Console.Write("2nd Input: ");
+                            Input2 = Int32.Parse(Console.ReadLine());
+
+                            int ReturnValue = Math.Division(Input1, Input2);
+                            Console.WriteLine("Return: " + ReturnValue);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error: " + ex.Message);
+                        }
+                        break;
+                    case "q":
+                    case "quit":
+                    case "Quit":
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Message: Invalid Command");
+                        break;
+                }
+            }  
+        }
+    }
+}
---
+++ UnitTests/MathTests.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleTestApp;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MathTests1
+    {
+        // Addition //
+        
+        [TestMethod]
+        public void Addition_Min()
+        {
+            int ReturnValue = Math.Addition(-1000000000, -1000000000);
+            Assert.AreEqual(ReturnValue, -2000000000);
+        }
+
+        [TestMethod]
+        public void Addition_Mid()
+        {
+            int ReturnValue = Math.Addition(5, 5);
+            Assert.AreEqual(ReturnValue, 10);
+        }
+
+        [TestMethod]
+        public void Addition_Max()
+        {
+            int ReturnValue = Math.Addition(1073741823, 1073741823);
+            Assert.AreEqual(ReturnValue, 2147483646);
+        }
+
+        /*[TestMethod]
+        public void Addition_Overflow()
+        {
+            try
+            {
+                int ReturnValue = Math.Addition(unchecked(1073741850), unchecked(1073741850));
+                //Assert.Ar(ReturnValue, 2147483700);
+            }
+            catch(System.Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Test");
+            }
+        }*/
+
+        // Subtraction //
+
+        [TestMethod]
+        public void Subtraction_Min()
+        {
+            int ReturnValue = Math.Subtraction(-1000000000, -1000000000);
+            Assert.AreEqual(ReturnValue, 0);
+        }
+
+        [TestMethod]
+        public void Subtraction_Mid()
+        {
+            int ReturnValue = Math.Subtraction(30, 10);
+            Assert.AreEqual(ReturnValue, 20);
+        }
+
+        [TestMethod]
+        public void Subtraction_Max()
+        {
+            int ReturnValue = Math.Subtraction(1073741823, 1073741800);
+            Assert.AreEqual(ReturnValue, 23);
+        }
+
+        /*[TestMethod]
+        public void Subtraction_Overflow()
+        {
+            try
+            {
+                int ReturnValue = Math.Addition(unchecked(1073741850), unchecked(1073741850));
+                //Assert.Ar(ReturnValue, 2147483700);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Test");
+            }
+        }*/
+
+        // Multiplication //
+
+        [TestMethod]
+        public void Multiplication_Min()
+        {
+            int ReturnValue = Math.Multiplication(-32767, -32767);
+            Assert.AreEqual(ReturnValue, 1073676289);
+        }
+
+        [TestMethod]
+        public void Multiplication_Mid()
+        {
+            int ReturnValue = Math.Multiplication(5, 5);
+            Assert.AreEqual(ReturnValue, 25);
+        }
+
+        [TestMethod]
+        public void Multiplication_Max()
+        {
+            int ReturnValue = Math.Multiplication(32767, 32767);
+            Assert.AreEqual(ReturnValue, 1073676289);
+        }
+
+        /*[TestMethod]
+        public void Multiplication_Overflow()
+        {
+            try
+            {
+                int ReturnValue = Math.Multiplication(unchecked(1073741850), unchecked(1073741850));
+                //Assert.Ar(ReturnValue, 2147483700);
+            }
+            catch(System.Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Test");
+            }
+        }*/
+
+        // Division //
+
+        [TestMethod]
+        public void Division_Min()
+        {
+            int ReturnValue = Math.Division(-1000000000, -1000000000);
+            Assert.AreEqual(ReturnValue, 1);
+        }
+
+        [TestMethod]
+        public void Division_Mid()
+        {
+            int ReturnValue = Math.Division(30, 10);
+            Assert.AreEqual(ReturnValue, 3);
+        }
+
+        [TestMethod]
+        public void Division_Max()
+        {
+            int ReturnValue = Math.Division(1073741000, 1000);
+            Assert.AreEqual(ReturnValue, 1073741);
+        }
+
+        [TestMethod]
+        public void Division_DivByZero()
+        {
+            try
+            {
+                int ReturnValue = Math.Division(1, 0);
+                Assert.IsTrue(false);
+            }
+            catch (System.Exception ex)
+            {
+                Assert.AreEqual(ex.Message, "Attempted to divide by zero.");
+            }
+        }
+    }
+}
